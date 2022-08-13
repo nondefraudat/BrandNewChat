4e26b063bd4ad4c7fe37c7098e0488f4c59b26e7
nondefraudat <nondefraudat@gmail.com>
2022-08-13 23:31:40 +0300

    Каркас, наверное

---
+++ src/main/java/ru/ntl/brandnewchat/BrandNewChatApplication.java
@@ -0,0 +1,13 @@
+package ru.ntl.brandnewchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandNewChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandNewChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ntl/brandnewchat/BrandNewChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ntl.brandnewchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandNewChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
